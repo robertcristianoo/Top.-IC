f63a2800e1383a87d3529fab93e162a8b4a71649
thetogmoc <rcristiano53@gmail.com>
2018-10-23 09:15:23 -0300

    Filtra alunos por universidade

--- Enade/Enade.py
+++ Enade/Enade.py
@@ -22,26 +22,6 @@ enade2017=pandas.read_csv("datasets\MICRODADOS_ENADE_2017.txt", sep=';',dtype={"
 
 enade2017.shape
 enade2017.columns[0:10]
-'''
-nota geral: 0-98 
-questão08: Qual a renda total de sua família, incluindo seus rendimentos?
-A = Até 1,5 salário mínimo (até R$ 1.405,50).
-B = De 1,5 a 3 salários mínimos (R$ 1.405,51 a R$ 2.811,00).
-C = De 3 a 4,5 salários mínimos (R$ 2.811,01 a R$ 4.216,50).
-D = De 4,5 a 6 salários mínimos (R$ 4.216,51 a R$ 5.622,00).
-E = De 6 a 10 salários mínimos (R$ 5. 622,01 a R$ 9.370,00).
-F = De 10 a 30 salários mínimos (R$ 9.370,01 a R$ 28.110,00).
-G = Acima de 30 salários mínimos (mais de R$ 28.110,00).
-
-i05 escolaridade da mae
-A = Nenhuma.
-B = Ensino fundamental: 1º ao 5º ano (1ª a 4ª série).
-C = Ensino fundamental: 6º ao 9º ano (5ª a 8ª série).
-D = Ensino médio.
-E = Ensino Superior - Graduação.
-F = Pós-graduação.
-
-'''
 
 tabela = pandas.DataFrame(enade2017, columns=['NT_GER', 'CO_CATEGAD', 'CO_GRUPO', 'QE_I08', 'CO_IES', 'QE_I05'])
 #print(tabela.head(10))
@@ -114,9 +94,35 @@ ccomp.QE_I05 = ccomp['QE_I05'].map({'A': 1, 'B': 2, 'C': 3, 'D': 4,'E': 5, 'F':6
 #plt.xlabel('Escolaridade da mãe.')
 #plt.show()
 
-escolaridade = ccomp.loc[ccomp.QE_I05 ==1]
+escolaridade = ccomp.loc[ccomp.QE_I05 == 1]
 #print(escolaridade.NT_GER.describe())
 
-federal_ti = tabela[tabela['CO_CATEGAD'] == 1]
-federal_ti = federal_ti.loc[tabela['CO_GRUPO'] == 4004]
-print(federal_ti.NT_GER.describe())
+#ALUNOS COMPUTAÇÃO FEDERAL
+cc_federal = tabela[tabela['CO_CATEGAD'] == 1]
+cc_federal = cc_federal.loc[tabela['CO_GRUPO'] == 4004]
+print(cc_federal.NT_GER.describe())
+
+#ALUNOS COMPUTAÇÃO NÃO FEDERAL
+cc_part = tabela[tabela['CO_CATEGAD'] != 1]
+cc_part = cc_part.loc[tabela['CO_GRUPO'] == 4004]
+print(cc_part.NT_GER.describe())
+
+#ALUNOS COMPUTAÇÃO ESTADUAL
+cc_part = tabela[tabela['CO_CATEGAD'] == 2]
+cc_part = cc_part.loc[tabela['CO_GRUPO'] == 4004]
+print(cc_part.NT_GER.describe())
+
+#ALUNOS COMPUTAÇÃO MUNICIPAL
+cc_part = tabela[tabela['CO_CATEGAD'] == 3]
+cc_part = cc_part.loc[tabela['CO_GRUPO'] == 4004]
+print(cc_part.NT_GER.describe())
+
+#ALUNOS COMPUTAÇÃO PRIVADA LUCRATIVA
+cc_part = tabela[tabela['CO_CATEGAD'] == 4]
+cc_part = cc_part.loc[tabela['CO_GRUPO'] == 4004]
+print(cc_part.NT_GER.describe())
+
+#ALUNOS COMPUTAÇÃO PRIVADA N LUCRATIVO
+cc_part = tabela[tabela['CO_CATEGAD'] == 5]
+cc_part = cc_part.loc[tabela['CO_GRUPO'] == 4004]
+print(cc_part.NT_GER.describe())
